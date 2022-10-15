337df64b414531ca0e0fb7ad252551bf0c6a5ae6
cvargascl <cvargascl@unjbg.edu.pe>
2022-10-14 21:36:17 -0500

    tipos de dato en pyton

---
+++ datatype.py
@@ -0,0 +1,39 @@
+#TIPOS DE DATOS
+
+#string
+from operator import truediv
+
+
+print("hello world")
+print('''hello world''')
+print("""hello world""")
+#funcion "type" para escribir el tipo de dato 
+print(type("hello world")) #tipo de dato "type"
+print("bye" + "world")   #Uniendo string; concadenación en programción, juntar cadenas de texto
+
+#integer 
+print(30)
+
+#float
+print(30.5)
+
+#Boolean
+True
+False
+
+#list
+[10, 20,30, 40, 50]
+["hello", "bye", True, 10.5] 
+
+#Tuples
+(10, 20, 30, 55 )
+()
+
+#Dictorionies
+print(type({
+    "name":"carlos",
+    "lastname" :"reyes",
+    "nickname": "castro"
+}))
+
+None
